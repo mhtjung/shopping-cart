afc2216ccd34f2c22aab01a11140a60b301ad52e
Mike Jung <themikejung@gmail.com>
2022-09-24 14:14:00 -0700

    Added Dockerfile and docker-compose.yml

--- server/index.js
+++ server/index.js
@@ -21,6 +21,7 @@ mongoose
 
 mongoose.Promise = global.Promise;
 app.use(morgan('tiny'));
+app.use(express.static('build'))
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "*");
